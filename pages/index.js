2777923c6fb2737e0313879d22ec8322e9812e1a
Alexandre Araujo Saura <alexandresaura21@gmail.com>
2025-11-12 00:42:49 +0000

    Add index page

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <blockquote>
+      Duas coisas são infinitas: o universo e a estupidez humana; e não tenho certeza sobre o universo.
+      - Albert Einstein
+    </blockquote>
+  );
+}
+
+export default Home;
